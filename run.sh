af50549b1c253291464999d4c9937aa22c572a9e
Roland Judd <rolandjudd@gmail.com>
2015-05-14 12:45:11 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./a.out caffeine learning_from_data experiments_in_physics the_essential cough_drops keystone dinner_plate tennis_ball
+./a.out caffeine_pills learning_from_data experiments_in_physics the_essential cough_drops keystone dinner_plate tennis_ball
 
