09740af5e384ba499c9939f0df46c2da855002e4
Roland Judd <rolandjudd@gmail.com>
2015-05-14 14:37:24 -0400

    Add snapple

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./a.out caffeine_pills learning_from_data experiments_in_physics the_essential cough_drops keystone dinner_plate tennis_ball advantage_card
+./a.out caffeine_pills learning_from_data experiments_in_physics the_essential cough_drops keystone dinner_plate tennis_ball advantage_card snapple
