e5ed3a3b85199e01702358969ddc0682bf35593a
Roland Judd <rolandjudd@gmail.com>
2015-05-14 12:01:39 -0400

    Change names of images

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./a.out caffiene ml_textbook physics_textbook book cough_drops keystone dinner_plate tennis_ball
+./a.out caffiene learning_from_data experiments_in_physics the_essential cough_drops keystone dinner_plate tennis_ball
 
